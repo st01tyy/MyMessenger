ff0bfccf8a9ff7ab27505287d1ed1ade20078ebf
st01tyy <tao_tyy@sina.com>
2019-07-12 16:32:11 +0800

    Update Eclipse Project Files

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application 
+{
+	/*
+	 * 主类 
+	 */
+	
+	@Override
+	public void start(Stage arg0) throws Exception 
+	{
+		/*
+		 * 程序启动入口
+		 */
+		
+		
+	}
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
