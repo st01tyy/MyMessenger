d8cb8bcccdc177bfae110d032a2d06026c218aef
st01tyy <44644324+st01tyy@users.noreply.github.com>
2019-07-27 19:18:42 +0800

    Update Register Function

--- src/controller/RegisterService.java
+++ src/controller/RegisterService.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -40,6 +41,10 @@ public class RegisterService
 			e.printStackTrace();
 		}
 	}
+	public static void cancelErrMsg(Label label)
+	{
+		label.setVisible(false);
+	}
 	public static void submitInput(String name, String pw)
 	{
 		
@@ -54,11 +59,19 @@ public class RegisterService
 			return 1;
 		if(pw.length() < 8 || pw.length() > 20)
 			return 2;
+		boolean hasNumber = false;
+		boolean hasCh = false;
 		for(int a = 0; a < pw.length(); a++)
 		{
 			if(pw.charAt(a) < 0 || pw.charAt(a) > 255)
 				return 2;
+			if(!hasCh && (pw.charAt(a) >= 'a' && pw.charAt(a) <= 'z') || (pw.charAt(a) >= 'A' && pw.charAt(a) <= 'Z'))
+				hasCh = true;
+			if(!hasNumber && pw.charAt(a) >= '0' && pw.charAt(a) <= '9')
+				hasNumber = true;
 		}
+		if(!hasNumber || !hasCh)
+			return 2;
 		if(!pwConfirm.equals(pw))
 			return 3;
 		return 0;
--- src/view/RegisterPannel.java
+++ src/view/RegisterPannel.java
@@ -4,6 +4,8 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXSpinner;
 import com.jfoenix.controls.JFXTextField;
+
+import controller.RegisterService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -51,6 +53,34 @@ public class RegisterPannel
     	 */
     	
     	image.setImage(new Image(this.getClass().getResourceAsStream("/resource/image/welcome.jpg")));
+    	txtfield_name.setOnMouseClicked(e -> RegisterService.cancelErrMsg(errmsg_name));
+    	txtfield_pw.setOnMouseClicked(e -> RegisterService.cancelErrMsg(errmsg_pw));
+    	txtfield_pwconfirm.setOnMouseClicked(e -> RegisterService.cancelErrMsg(errmsg_pwconfirm));
+    	btn_register.setOnMouseClicked(e -> onSubmitButtonClicked());
     }
-
+    
+    private void onSubmitButtonClicked()
+    {
+    	/*
+    	 * 按钮的事件反应器。
+    	 */
+    	
+    	int res = RegisterService.judgeInput(txtfield_name.getText(), txtfield_pw.getText(), txtfield_pwconfirm.getText());
+    	if(res != 0)
+    	{
+    		txtfield_pw.setText("");
+    		txtfield_pwconfirm.setText("");
+    		if(res == 1)
+    			errmsg_name.setVisible(true);
+    		else if(res == 2)
+    			errmsg_pw.setVisible(true);
+    		else
+    			errmsg_pwconfirm.setVisible(true);
+    	}
+    	else
+    	{
+    		
+    	}
+    }
+    
 }
